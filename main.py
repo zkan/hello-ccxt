d2012c449ee0ae70aa8d781e4cbdbf7ae3ec279e
zkan <zkan.cs@gmail.com>
2021-10-06 20:18:05 +0700

    Get Shiba price :bear:

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ symbol = "BTC/USDT"
 timeframe = "1h"
 limit = 100
 
-# Open, High, Low, Close and Volume
-ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
-print(ohlcv)
-
+# # Open, High, Low, Close and Volume
+# ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
+# print(ohlcv)
+#
 exchange_id = "binance"
 exchange_class = getattr(ccxt, exchange_id)
 exchange = exchange_class()
 ohlcv = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
+print(ohlcv)
+
+timeframe = "1m"
+shiba_symbol = "SHIB/USDT"
+ohlcv = exchange.fetch_ohlcv(shiba_symbol, timeframe=timeframe, limit=5)
 print(ohlcv)
